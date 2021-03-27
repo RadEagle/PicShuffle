4b30a366b6ade6f39220d10d60451784c495606d
Jonathan Chau <31170739+RadEagle@users.noreply.github.com>
2021-03-27 10:11:26 -0700

    Add files via upload

---
+++ renamer.py
@@ -0,0 +1,35 @@
+import os
+import string
+import random
+
+
+def get_random_string(length):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
+
+
+def rename_file(file_name, phrase):
+    extension = os.path.splitext(file)[1]
+    new_name = phrase + extension
+    os.rename(file_name, new_name)
+
+
+# Input directory
+dir_name = input("Enter directory here: ")
+
+# Process directory
+directory = os.listdir(dir_name)
+dir_size = len(directory)
+os.chdir(dir_name)
+nums = random.sample(range(1, dir_size + 1), dir_size)
+
+# To prevent loss of files, rename each file to strings of characters
+for file in directory:
+    rename_file(file, get_random_string(5))
+
+# Rename each file in numerical order
+count = 0
+directory = os.listdir('.')
+for file in directory:
+    rename_file(file, str(nums[count]))
+    count = count + 1
